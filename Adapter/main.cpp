fd01d30b608a2bec681cd01193fc66ffbedbf9a0
k-silver <silver@k-silver.org>
2018-11-03 12:52:55 +0800

    适配器模式

---
+++ Adapter/main.cpp
@@ -0,0 +1,14 @@
+#include "Adapter.h"
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) 
+{
+	// Adapter* adt = new Adapter();
+	Target* adt = new Adapter();
+
+	adt->Request();
+
+	return 0;
+}
